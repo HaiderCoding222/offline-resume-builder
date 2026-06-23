78c177d2c8351d6c3a79c8c230207dce1ef6eed9
HaiderCoding222 <lak355603@gmail.com>
2026-06-23 10:43:42 +0500

    Add files via upload

---
+++ template-config.js
@@ -0,0 +1,10 @@
+const AVAILABLE_TEMPLATES = [
+    {
+        id: "default",
+        name: "Classic Minimalist",
+        htmlFile: "template-default.html",
+        cssFile: "template-default.css",
+        scriptFile: "template-default.js",
+        thumbnail: "images/temp-img-1.png"     
+    }
+];
